e9462389aa42efeeb9f450810531d785e0dcc125
Oleg Evseev <git@x3n.me>
2022-05-24 22:33:04 +0400

    Minor changes to index.tsx

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,4 +1,4 @@
-/// <reference types="telegram-web-app" />
+/// <reference types="telegram-web-app"/>
 
 import "./style.css";
 
@@ -6,7 +6,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import interact from "interactjs";
 
-const content = <div>
+const content = <div className="container mx-auto p-4">
     <h1>Welcome to the web app, {window.Telegram.WebApp.initDataUnsafe.user.username}!</h1>
     <h2 className="hint">Have a nice day!</h2>
     <pre>{JSON.stringify(window.Telegram.WebApp, null, 2)}</pre>
