3bc6912583ea8944b3463a0dd0c6d64e0697fea3
Oleg Evseev <git@x3n.me>
2022-06-04 13:54:28 +0400

    Restructure webapp React way

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -5,7 +5,47 @@ import "./style.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
-const content = <div></div>;
+interface PreferansState {
+}
+
+interface PreferansViewHandlers {
+}
+
+interface PreferansViewProps {
+    gameState: PreferansState;
+    handlers: PreferansViewHandlers;
+}
+
+class PreferansView extends React.Component<PreferansViewProps, {}> {
+    render(): JSX.Element {
+        return <div>{JSON.stringify(this.props.gameState)}</div>;
+    }
+}
+
+interface AppConfig {
+}
+
+interface AppState {
+    gameState: PreferansState;
+}
+
+class App extends React.Component<{config: AppConfig}, AppState> {
+    handlers: PreferansViewHandlers;
+
+    constructor(props: {config: AppConfig}) {
+        super(props);
+        this.state = {
+            gameState: {}
+        };
+        this.handlers = {};
+    }
+
+    render(): JSX.Element {
+        return <PreferansView gameState={this.state.gameState} handlers={this.handlers}/>;
+    }
+}
+
+const appConfig = {};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(content);
+root.render(<App config={appConfig}/>);
